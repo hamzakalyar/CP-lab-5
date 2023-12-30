3f4dceb96474b3c199567990b9a07a4d8228472e
hamzakalyar <146337680+hamzakalyar@users.noreply.github.com>
2023-12-31 01:29:21 +0500

    Add project files.

---
+++ CP lab 5/Extra task 1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a = 7;
+	for (int i = 1; i <= 14; i++) {
+		cout << a * i << " ";
+	}
+
+}
---
+++ CP lab 5/Extra task 2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+	int a = 1;
+	for (int i = 1; i <= 10; i++) {
+		cout << a;
+		a *= 2;
+	}
+		
+}
